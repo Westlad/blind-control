fa5f1c4ccadcf150a16c45d42ad0fb639b3f925b
duncan <duncan@duncanwestland.com>
2018-05-13 17:39:28 +0100

    tweaks to code to improve reliability

--- blinds/blinds.ino
+++ blinds/blinds.ino
@@ -9,23 +9,23 @@
 
 #include <LwRx.h>
 
-const int ZERO = 398; //the time for a zero bit in us
-const int ONE = 785; //the time for a one bit in us
+const int ZERO = 390; //the time for a zero bit in us
+const int ONE = 787; //the time for a one bit in us
 const int SHORT = 4408; //the time for the short low before data transmission
 const int LONG = 28; //ms not us
-const int SYNC = 12; //number of sync pulses to send: try 11 if this doesn't work
+const int SYNC = 11; //number of sync pulses to send: try 11 if this doesn't work
 const int BITS = 48;
 //the Pellinin raise and lower codes
-const byte RAISE[BITS] = {1,1,1,1, 1,1,1,1, 1,1,1,0, 0,1,0,0, 0,0,0,0, 0,0,1,0, 1,1,1,0, 0,0,1,0, 0,0,0,0, 1,1,1,1, 0,1,1,0, 0,0,0,1};
-const byte LOWER[BITS] = {1,1,1,1, 1,1,1,1, 1,1,1,1, 0,0,0,0, 0,0,0,0, 0,0,1,0, 1,1,1,0, 0,0,1,0, 0,1,0,0, 1,1,1,1, 0,1,1,0, 0,1,0,1};
+byte RAISE[BITS] = {1,1,1,1, 1,1,1,1, 1,1,1,0, 0,1,0,0, 0,0,0,0, 0,0,1,0, 1,1,1,0, 0,0,1,0, 0,0,0,0, 1,1,1,1, 0,1,1,0, 0,0,0,1};
+byte LOWER[BITS] = {1,1,1,1, 1,1,1,1, 1,1,1,1, 0,0,0,0, 0,0,0,0, 0,0,1,0, 1,1,1,0, 0,0,1,0, 0,1,0,0, 1,1,1,1, 0,1,1,0, 0,1,0,1};
 const byte msglen = 10;
 //The codes that will be received from the LightwaveRF hub
-const byte OPEN[msglen] = {1, 15, 1, 7, 0, 4, 8, 1, 7, 0};
-const byte CLOSE[msglen] = {0, 0, 1, 7, 0, 4, 8, 1, 7, 0};
+byte OPEN[msglen] = {1, 15, 1, 7, 0, 4, 8, 1, 7, 0};
+byte CLOSE[msglen] = {0, 0, 1, 7, 0, 4, 8, 1, 7, 0};
 const int OUT = 3; //the pin connected to the tx
 const int GREEN = 7; //green LED
 const int RED = 8; //red LED
-const unsigned int OP_TIME = 15000; //the time that the lower/raise command will be repeated for (simulates a button press-and-hold)
+const unsigned long OP_TIME = 15000; //the time that the lower/raise command will be repeated for (simulates a button press-and-hold)
 
 const int RX_PIN = 2; //pin to receive rx data on
 const int RX_POWER = 4; // connect to receiver Vcc
@@ -41,7 +41,7 @@ void setup() {
   pinMode(RED, OUTPUT);
   pinMode(RX_POWER, OUTPUT);
   digitalWrite(RX_POWER, HIGH);
-  Serial.begin(19200);
+  //Serial.begin(19200);
   lwrx_setup(RX_PIN);
   t = millis();
 }
@@ -49,13 +49,17 @@ void setup() {
 void loop() {
   if (lwrx_message()) { //we have received a message from the LightwaveRF hub
     lwrx_getmessage(msg, msglen);
-    digitalWrite(RX_POWER, LOW); //the transmitter causes interrupts
-    digitalWrite(OUT, HIGH); //turn the TX on
+
     opn = true;
     cse = true;
     for (int i =0; i<msglen; i++){ //is it an up or a down message?
       if (msg[i] != CLOSE[i]) cse = false;
       if (msg[i] != OPEN[i]) opn = false;
+    }
+    if (opn || cse){ //this is a message we are interested in
+      digitalWrite(RX_POWER, LOW); //the transmitter causes interrupts so turn RX off
+      digitalWrite(OUT, HIGH); //turn the TX on
+      delay(2000);  //wait to ensure that the rf hub has stopped sending
     }
     if (opn) {
       digitalWrite(GREEN,HIGH);
@@ -134,6 +138,7 @@ void sendData(int pin, byte data[]){
   }
 }
 
+/**
 void printMsg(byte *msg, byte len) {
    for(int i=0;i<len;i++) {
       Serial.print(msg[i]);
@@ -141,7 +146,7 @@ void printMsg(byte *msg, byte len) {
    }
    Serial.println();
 }
-
+*/
 void flash(byte pin) {
    digitalWrite(pin, HIGH);   // turn the LED on (HIGH is the voltage level)
    delay(100);                       // wait for a second
